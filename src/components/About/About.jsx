d85e9449c64d7a7bf65e50d49be77eae8e8a3cbd
sachinchaudhary1999 <sachinchaudhary9794@gmail.com>
2026-05-13 21:04:52 +0530

    improvement in Profile section

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactTypingEffect from "react-typing-effect";
 import Tilt from "react-parallax-tilt";
-import profileImage from "../../assets/profile1.png";
+import profileImage from "../../assets/Profile.webp";
 
 const About = () => {
   return (
